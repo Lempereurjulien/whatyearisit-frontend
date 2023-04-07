4b5e2f813b564ba76f2a021cafcb67c98793e8b1
Lempereur <lempereur.julien83@gmail.com>
2023-04-07 11:56:38 +0200

    s

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@
 fetch('https://whatyearisit-backend-lake-xi.vercel.app/year')
 .then(response => response.json())
 .then(data => {
+    console.log(data)
     document.querySelector('#year').textContent =
     `${data}`
 })
